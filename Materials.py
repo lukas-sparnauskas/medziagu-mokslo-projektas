a965ce11e19dd515e29e36242c6438b1727f707d
DarTas1 <dartas1985244@gmail.com>
2020-12-17 00:04:50 +0200

    Signed-off-by: DarTas1 <dartas1985244@gmail.com>

--- Materials.py
+++ Materials.py
@@ -14,7 +14,13 @@ class Materials:
         Material(0, "Varis", 0.0000167, material_info.varis),
         Material(1, "Auksas", 0.0000142, material_info.auksas),
         Material(2, "Geležis", 0.000012, material_info.gelezis),
-        Material(3, "Aliuminis", 0.000023, material_info.aliuminis)
+        Material(3, "Aliuminis", 0.000023, material_info.aliuminis),
+        Material(4, "Gintaras", 0.000055, material_info.gintaras),
+        Material(5, "Bronza", 0.0000177, material_info.bronza),
+        Material(6, "Kalcis", 0.0000223, material_info.kalcis),
+        Material(7, "Kobaltas", 0.000012, material_info.kobaltas),
+        Material(8, "Platina", 0.000009, material_info.platina),
+        Material(9, "Cirkonis", 0.0000057, material_info.cirkonis)
 
         # Jei norit įdėti medžiagą, į šitą listą įdėkit dar vieną Material() su id (+1), vardu, alfa koeficientu ir medžiagos info (aprašytą faile material_info.py)
     ]
--- material_info.py
+++ material_info.py
@@ -1,4 +1,52 @@
-varis = "vario info"
-auksas = "aukso info"
-gelezis = "gelezies info"
-aliuminis = "aliuminio info"
+varis = """Varis (Cuprum; Cu) – periodinės elementų sistemos I grupės cheminis elementas. Metalas.
+
+Varis – grynuolių klasės mineralas. Gamtoje randamas varis turi geležies, sidabro, rečiau aukso priemaišų. Kristalai kubinės singonijos, kubų, rombinių dodekaedrų ir oktaedrų formos. Varis dažniausiai sudaro dendritus, plokštelių, lapelių, vielos pavidalo agregatus, konkrecijas. Blizgesys metalinis. Daugiausia susidaro hidroterminėmis ir egzogeninėmis sąlygomis. Randamas sulfidinėse ir kitose vario rūdose Rusijoje, Kazachstane, JAV, Čilėje, PAR.
+
+Daugiau kaip 50 % vario suvartojama elektrotechnikos pramonėje (folija, laidai, kabeliai, elektriniai kontaktai, iš vario daromos šaldytuvų, šilumokaičių, vakuuminių prietaisų detalės (geras šilumos laidininkas, atsparus korozijai)). Varis vartojamas papuošalams gaminti ir dekoratyvinei apdailai. Vario junginiai vartojami pigmentų, insekticidų, mikrotrąšų, katalizatorių gamyboje. Pagal vartojimą varis yra trečias metalas (po geležies ir aliuminio)."""
+auksas = """Auksas – cheminis periodinės elementų lentelės elementas, žymimas Au (lot. aurum), eilės numeris 79, taurusis metalas.
+
+Auksas yra ryškiai geltonas taurusis metalas, dėl priemaišų gali būti įvairių atspalvių (lydiniuose – nuo vario raudonumo iki sidabriškai balto). Yra minkštas, tąsus ir kalus: lengvai ištempiama aukso viela, išplojami plonyčiai (peršviečiami) lakštai (folija). Elektrinis ir šilumos laidumas ~30 % mažesnis negu sidabro.
+
+Auksas ir jo lydiniai naudojami monetų ir juvelyrinių dirbinių, medalių, dantų protezų, chem. aparatūros gamybai. Radiotechnikoje ir elektronikoje naudojami auksu padengti kontaktai ir laidai, izotopu 198Au gydomos vėžinės ligos."""
+gelezis = """Geležis – cheminis periodinės elementų lentelės elementas, žymimas Fe (lot. ferrum), eilės numeris 26, sidabriškai pilkas, blizgus metalas. Tai gausiausias metalas Žemėje (Žemės plutoje – antras pagal gausumą, pirmas – aliuminis – 8 %) ir manoma, kad 10 pagal gausumą elementas visatoje. Geležis – didžiausią Žemės dalį pagal masę sudarantis elementas (34,6 %), taip pat sudaro 4,65 % Žemės plutos masės. Manoma, kad didelė geležies koncentracija labiausiai veikia magnetinį Žemės lauką.
+
+Geležis ir jos lydiniai yra plačiausiai naudojamas metalas (jie sudaro 95 % visų metalinių gaminių masės). Toks populiarumas susijęs su geležies santykiniu pigumu ir tvirtumu, dėl kurio ją galima panaudoti įvairiose srityse, įskaitant automobilių pramonę, laivų statybą, statybinių konstrukcijų gamybą.
+
+Geležies pagrindu kuriamos naujos medžiagos, atsparios aukštai arba žemai temperatūrai, vakuumui ar dideliam slėgiui, agresyvioms aplinkoms, didelėms kintamoms apkrovoms, jonizuojantiems spinduliams."""
+aliuminis = """Aliuminis (lot. Aluminium) – cheminis periodinės elementų lentelės elementas, žymimas Al, eilės numeris 13. Lengvas, minkštas, plastiškas, kalus, tąsus, lengvai poliruojamas, anoduojamas, atspindintis šviesą.
+
+Iš aliuminio gaminama iki 0,004 µm folija (valcuojant), kalant gaminami lakštai. Aliuminiu dengiamas plienas apsaugai nuo korozijos, paprasti ir optinių teleskopų veidrodžiai, kaip reduktorius ir deoksidatorius naudojamas lydant plieną ar ketų. Aliuminio pudra naudojama gaminant akytąjį betoną, dažų ir lako pramonėje suteikiant jiems žvilgesį, folija – maisto produktams pakuoti. Skysto pavidalo junginiai naudojami vakcinose, gaminant padidinto atsparumo ugniai medžiagas."""
+gintaras = """Gintaras – fosiliniai sakai. Tai organinės kilmės mineralas, susidaręs sustingus spygliuočių augalų sakams. Dėl savo spalvos ir natūralaus grožio vertintas ir naudotas papuošalams nuo neolito.
+
+Sudarytas iš anglies, deguonies ir vandenilio. Paprastai jame yra 79% C, 10,5% O ir 10,5% H. Pagal O. Helmą, gintaras turi nuo 3 iki 8% gintaro rūgšties.
+
+Kietumas pagal Moso skalę – 2-2,5, retkarčiais padidėja iki 3.
+
+Pagrinde naudojamas juvelyriniuose dirbiniuose"""
+bronza = """Bronza – vario lydinys su alavu, arsenu ar kitais metalais bei pusmetaliais, išskyrus lydinius su cinku (žalvarį) ir nikeliu. Taip pat į lydinį dažnai įmaišoma cinko ar švino.
+
+Atsižvelgiant į pagrindinį legiravimo elementą, bronzas įprasta skirstyti į dvi grupes: alavinės bronzos, bealavės bronzos (aliumininės, silicinės, berilinės bei kitos).
+
+Bronza yra naudojama architektūroje konstrukciniams ir konstrukciniams elementams, dėl guolių dėl savo trinties savybių ir kaip fosforo bronzos muzikos instrumentuose, elektros kontaktuose ir laivo sraigtuose. Aliuminio bronza yra naudojama staklėms gaminti ir kai kuriems guoliams."""
+kalcis = """Kalcis (lot. Calcium, Ca), periodinės elementų sistemos II grupės šarminių žemių metalų pogrupio cheminis elementas. Atomų skaičius 20, atomų masė 40,08. Išorinių elektronų konfigūracija 4s². Oksidacijos laipsnis junginiuose +2. Gamtinis kalcis yra 6 stabilių izotopų mišinys, didžiausią dalį (96,97 %) sudaro 40Ca. Žemės plutoje kalcio yra 2,96 % (pagal masę), tai penktas pagal gausumą elementas (po deguonies, silicio, aliuminio ir geležies). Gamtoje randamas tik mineralų pavidalu.
+
+Kalciu metalotermijoje redukuojami metalai (uranas, toris, chromas, vanadis), iš plieno šalinamas deguonis ir siera, valomi naftos produktai, iš inertinių dujų šalinamos azoto priemaišos. Elektrovakuuminiuose prietaisuose kalcis vartojamas kaip geteris. Kalcio dedama į anti-frikcinius lydinius.
+
+Daugiausiai kalcio yra suaugusio žmogaus kauluose ir kraujo plazmoje. Maisto produktai, kuriuose yra oksalatų (rūgštynės, rabarbarai, špinatai), fosfatų ir fitino (miltai, kruopos), trukdo organizmui pasisavinti kalcį, gali netgi susidaryti inkstų akmenys. Riebiosios rūgštys, esančios žarnyne, su kalciu taip pat sudaro netirpstančius junginius, vadinamus kalcio muilu, ir tai trukdo pasisavinti kalcį.
+
+Kalcio kiekį organizme reguliuoja vitaminas D, prieskydinės ir skydinės liaukų hormonai. Jei yra kalcio trūkumas, suminkštėja kaulai, atsiranda rizika susirgti rachitu.
+
+Kalcis mažina nervų sistemos dirglumą, reguliuoja kapiliarų bei ląstelių membranų laidumą, kraujo krešėjimo procesus. Kalcio yra rupaus malimo miltuose, kopūstuose, žemuogėse, kriaušėse, slyvose, piene bei jo produktuose ir kt."""
+kobaltas = """Kobaltas (lot. Cobaltum, Co, periodinės elementų sistemos VIII B grupės cheminis elementas. Sidabriškas, truputį gelsvas, rausvo ar melsvo atspalvio metalas. Būna dviejų atmainų: alfa kobaltas (iki 427 °C, heksagoninė kristalinė gardelė) ir beta kobaltas (aukštesnėje negu 427 °C, kubinė centruotojo paviršiaus gardelė). Feromagnetikas. Kompaktiškas kobaltas atsparus oro poveikiui, labai smulkus – piroforiškas.
+
+Daugiausia kobaltas naudojamas lydiniams (su geležimi, nikeliu, variu, volframu ir kitais metalais), kurie naudojami kaip magnetinės medžiagos, pjovimo įrankiams, raketų varikliams ir dujų turbinoms gaminti."""
+platina = """Platina (lot. Platinum), Pt – periodinės elementų lentelės VIII grupės cheminis elementas, Platinos grupės metalas. Atominis skaičius 78, atominė masė 195,09. Gamtinę platiną sudaro 6 izotopai, iš jų daugiausia yra 195Pt (33 %), ir 194Pt (32,9 %). Platina sudaro 1*10-6 Žemės plutos masės. Platina – grynuolių klasės mineralas. Paprastai turi iki 35 % paladžio, geležies, iridžio, osmio, vario, nikelio, radžio, aukso priemaišų. Kristalai (pasitaiko retai) kubinės singonijos. Platina dažniausiai sudaro spindulinius arba žvynelinių pavidalo agregatus. Kietumas pagal Moso skalę – 4. Randama ultrabazinėse uolienose, sąnašynuose.
+
+Iš platinos ir jos lydinių gaminami atsparūs korozijai ir cheminiams reagentams indai, aparatūra, daromi elektrodai ir el. kontaktai, varžiniai termometrai ir termoporos, chirurginiai instrumentai, filjerės. Platina vartojama kaip katalizatorius gaminant sieros ir azoto rūgštį, angliavandenilius, vandenilį, hidrinant riebalus, sintetinant vaistus. Iš platinos bei iridžio lydinio gaminami metro ir kilogramo etalonai.
+
+Iš platinos kaldinamos atmintinės platinos monetos."""
+cirkonis = """Cirkonis (lot. Zirconium) – cheminis periodinės elementų lentelės IVB grupės elementas, metalas, žymimas Zr, eilės numeris 40. Plastiškas, lengvai kalamas. Kambario temperatūroje cirkonį dengia plona tvirta oksido plėvelė, todėl atsparus korozijai bei rūgščių poveikiui. Aukštoje temperatūroje sugeria vandenilį sudarydamas cirkonio hidridus.
+
+Gamtoje randamas tik junginiuose, mineralai reti. Chloridiniu ar šarminiu būdu gaunamas iš rūdų. Kristalinės atmainos – alfa heksagoninis ir kubinis beta, iš pirmojo į antrąjį virsta 863 °C temperatūroje.
+
+Cirkonio halogenidai naudojami gaminant šviesolaidžius, lazerius, specialų stiklą, cheminius jutiklius. Cirkonio nitridas (Zr3N4) naudojamas gaminant keramiką, antifrikcines bei apsaugines dangas. Karbidu (ZrC) dengiami metalai, iš jo gaminami ugniai atsparūs lydiniai. Cirkonio oksidas ZrO2 randamas mineralo badeleito pavidalu ir naudojamas gaminant keramiką, stiklą, emalį, abrazyvą, aukštos temperatūros elektrodus, taip pat naudojamas optiniuose elektros prietaisuose, stomatologijoje, aviacijoje, raketinėje technikoje, o monokristalai – gaminant lazerius bei juvelyrikoje. Mineralas cirkonas (ZrSiO4) naudojamas uolienų datavimui, taip pat juvelyrikoje."""
