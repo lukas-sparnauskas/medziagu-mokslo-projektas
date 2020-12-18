446854cf6b521aa93a415316503aa74329f1ae3e
lukas-sparnauskas <lukas.11sp@gmail.com>
2020-12-18 23:54:45 +0200

    exportintas exe ir pataisyta ataskaita

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ import pyqtgraph as pg
 import numpy as np
 import sys
 
+def resource_path(relative_path):
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
 ####################################################
 ##  Langų dydžiai
 global mainWindowX
